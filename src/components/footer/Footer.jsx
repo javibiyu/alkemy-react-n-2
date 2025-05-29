98e55133c67c4291a950a91e1ba73450f86c0059
Javier Librandi <javierhernan.librandi@bbva.com>
2025-05-29 18:10:10 -0300

    agrego el footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
-
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="w-full bg-gray-900 text-white py-4 mt-8">
+      <hr className="border-gray-700 mb-5 mt-0" style={{ marginTop: "5px" }} />
+      <div className="container mx-auto text-center">
+        © {new Date().getFullYear()} Cosme Fulanito. Todos los derechos
+        reservados.
+      </div>
+    </footer>
+  );
 }
