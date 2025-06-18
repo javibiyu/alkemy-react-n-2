fc41f907bdc1da38bb674e6c4f14afaf408b43c8
Javier Librandi <javierhernan.librandi@bbva.com>
2025-06-18 13:36:10 -0300

    Corrigo problemas con el css

--- src/components/routers/MainRout.jsx
+++ src/components/routers/MainRout.jsx
@@ -11,16 +11,18 @@ import Layout from "../ui/layouts/Layout";
 export default function MainRout() {
   return (
     <Router>
-      <Layout />
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/usuarios" element={<UsuariosPage />} />
-        <Route path="/productos" element={<ProductosPage />} />
-        <Route path="/producto/:id" element={<ProductoDetalle />} />{" "}
-        <Route path="/nuevoproducto" element={<AgregarProducto />} />{" "}
-        {/* Ruta dinámica */}
-        <Route path="*" element={<NotFound />} /> {/* Ruta para el error 404 */}
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/usuarios" element={<UsuariosPage />} />
+          <Route path="/productos" element={<ProductosPage />} />
+          <Route path="/producto/:id" element={<ProductoDetalle />} />{" "}
+          <Route path="/nuevoproducto" element={<AgregarProducto />} />{" "}
+          {/* Ruta dinámica */}
+          <Route path="*" element={<NotFound to="404" replace />} />{" "}
+          {/* Ruta para el error 404 */}
+        </Routes>
+      </Layout>
     </Router>
   );
 }
--- src/components/ui/layouts/Layout.jsx
+++ src/components/ui/layouts/Layout.jsx
@@ -4,11 +4,9 @@ import Navbar from "../../navBar/NavBar";
 export default function Layout({ children }) {
   return (
     <>
-      <section>
-        <Navbar />
-        <div style={{ minHeight: "75.4vh" }}>{children}</div>
-        <Footer />
-      </section>
+      <Navbar />
+      <div style={{ minHeight: "75.4vh" }}>{children}</div>
+      <Footer />
     </>
   );
 }
