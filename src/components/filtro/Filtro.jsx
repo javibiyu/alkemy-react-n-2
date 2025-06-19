ecc51e95174d927dc0bee6becbc27f391bd13cbb
Javier Librandi <javierhernan.librandi@bbva.com>
2025-06-19 16:38:13 -0300

    configurando el filtro

--- src/components/filtro/Filtro.jsx
+++ src/components/filtro/Filtro.jsx
@@ -1,7 +1,21 @@
 import { TextField } from "@mui/material";
 
-export default function Filtro() {
+export default function Filtro({ colection, field }) {
+  const handleChange = (event) => {
+    const value = event.target.value;
+    const filtrados = colection?.filter((item) => {
+      return item[field]?.toLowerCase().includes(value.toLowerCase());
+    });
+    console.log(filtrados);
+  };
+
   return (
-    <TextField fullWidth id="buscar-basic" label="Buscar" variant="standard" />
+    <TextField
+      fullWidth
+      id="buscar-basic"
+      label="Buscar"
+      variant="standard"
+      onChange={handleChange}
+    />
   );
 }
